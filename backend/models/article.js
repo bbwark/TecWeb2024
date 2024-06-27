c3cb739f1f2379b921717253fc9357914b85d131
bbwark <devivobruno01@gmail.com>
2024-06-27 22:36:43 +0200

    Sequelize data models added

---
+++ backend/models/article.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const Article = sequelize.define('Article', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    });
+    return Article;
+};
