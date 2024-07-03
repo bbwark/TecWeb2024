0e1689bd41f19e7a0b6fc218b890e22d15f8e44a
bbwark <devivobruno01@gmail.com>
2024-07-03 21:59:53 +0200

    Further Implementation ArticleDetail.js

--- frontend/static/js/views/ArticleDetail.js
+++ frontend/static/js/views/ArticleDetail.js
@@ -1,14 +1,33 @@
 import AbstractView from "./AbstractView.js";
+import HeaderDetail from "./components/HeaderDetail.js";
 
 export default class extends AbstractView {
-    constructor(params) {
-        super(params);
-        this.setTitle("Article Detail");
-    }
+  constructor(params) {
+    super(params);
+    this.setTitle("Article Detail");
+    this.article = params.article;
+  }
 
-    async getHtml() {
-        return `
-            <h1>Article Detail</h1>
+  async getHtml() {
+    const headerView = new HeaderDetail({
+      isLogged: this.params.isLogged,
+      isOwner: this.params.isOwner,
+      isAdmin: this.params.isAdmin,
+    });
+    const headerHtml = await headerView.getHtml();
+
+    return `
+        <div class="article-detail">
+            <h2>${this.article.title}</h2>
+            ${headerHtml}
+            <div class="article-detail-subtitle">
+                <p>Author: ${this.article.author}</p>
+                <p>Published on: ${this.article.publishedDate}</p>
+                <p>Last modified: ${this.article.modifiedDate}</p>
+            </div>
+            <p>${this.article.content}</p>            
+            <p>Tags: ${this.article.tags.join(", ")}</p>
+        </div>
         `;
-    }
+  }
 }
