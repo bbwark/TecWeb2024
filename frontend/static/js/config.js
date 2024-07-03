9995cb408f3ea47ca8eaf162d952cbef557a1992
bbwark <devivobruno01@gmail.com>
2024-07-03 22:00:10 +0200

    Config now containse also state of the application

--- frontend/static/js/config.js
+++ frontend/static/js/config.js
@@ -1,6 +1,26 @@
 const config = {
-    apiUrl: "https://api.example.com",
-    numberOfArticles: 10 // Number for pagination
+    numberOfArticles: 10, // Number for pagination
+    apiBaseUrl: "https://api.example.com"
 };
 
-export default config;
+const state = {
+    isLoggedIn: false,
+    isAdmin: false,
+    user: null,
+    setLoggedInStatus(status) {
+        this.isLoggedIn = status;
+    },
+    setAdminStatus(status) {
+        this.isAdmin = status;
+    },
+    setUser(user) {
+        this.user = user;
+    },
+    clearUser() {
+        this.user = null;
+        this.isLoggedIn = false;
+        this.isAdmin = false;
+    }
+};
+
+export { config, state };
