8d13e3494fc84a2cd0d2ab12138cb2458a0bc3c6
bbwark <devivobruno01@gmail.com>
2024-07-03 21:58:38 +0200

    Header refactor

---
+++ frontend/static/js/views/components/HeaderDetail.js
@@ -0,0 +1,31 @@
+import AbstractView from "../AbstractView.js";
+
+export default class extends AbstractView {
+  constructor(params) {
+    super(params);
+  }
+
+  async getHtml() {
+    const { isLogged, isOwner, isAdmin } = this.params;
+    return `
+        <div id="header">
+            ${
+              isLogged
+                ? `
+                ${
+                  isOwner || isAdmin
+                    ? `
+                <button id="delete">Delete</button>
+                <button id="modify">Modify</button>`
+                    : ""
+                }
+                <button id="settings">Settings</button>
+                `
+                : `
+                <button id="login-button">Login</button>
+                `
+            }
+        </div>
+    `;
+  }
+}
