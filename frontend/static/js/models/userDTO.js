c8f80c890b169cd3b0de444ee79b9918d348adb0
Bruno De Vivo <devivobruno01@gmail.com>
2024-07-04 15:29:46 +0200

    User model file for handling users

---
+++ frontend/static/js/models/userDTO.js
@@ -0,0 +1,8 @@
+export default class UserDTO {
+    constructor(id, username, name, isAdmin) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.isAdmin = isAdmin;
+    }
+}
