ece9e1adc2d1b137e8bf25c3bd47201a7148edfb
bbwark <devivobruno01@gmail.com>
2024-07-11 20:10:47 +0200

    Crud Operations Added

---
+++ backend/crud/articleCrud.js
@@ -0,0 +1,59 @@
+const { Article } = require("../databaseconn");
+
+const createArticle = async (articleData) => {
+  return await Article.create(articleData);
+};
+
+const getArticleById = async (articleId) => {
+  return await Article.findByPk(articleId);
+};
+
+const getAllArticles = async () => {
+  return await Article.findAll();
+};
+
+const getRecentArticles = async (limit, offset) => {
+  return await Article.findAll({
+    limit: limit,
+    offset: offset,
+    order: [["createdAt", "DESC"]],
+  });
+};
+
+const getRecentArticlesByTag = async (tag, limit, offset) => {
+  return await Article.findAll({
+    where: {
+      tags: tag,
+    },
+    limit: limit,
+    offset: offset,
+    order: [["createdAt", "DESC"]],
+  });
+};
+
+const updateArticle = async (articleId, updateData) => {
+  const article = await Article.findByPk(articleId);
+  if (article) {
+    return await article.update(updateData);
+  }
+  return null;
+};
+
+const deleteArticle = async (articleId) => {
+  const article = await Article.findByPk(articleId);
+  if (article) {
+    await article.destroy();
+    return true;
+  }
+  return false;
+};
+
+module.exports = {
+  createArticle,
+  getArticleById,
+  getAllArticles,
+  getRecentArticles,
+  getRecentArticlesByTag,
+  updateArticle,
+  deleteArticle,
+};
---
+++ backend/crud/userCrud.js
@@ -0,0 +1,47 @@
+const { User } = require("../databaseconn");
+
+const createUser = async (userData) => {
+  return await User.create(userData);
+};
+
+const getUserById = async (userId) => {
+  return await User.findByPk(userId);
+};
+
+const getAllUsers = async () => {
+  return await User.findAll();
+};
+
+const getRecentUsers = async (limit, offset) => {
+  return await User.findAll({
+    limit: limit,
+    offset: offset,
+    order: [["updatedAt", "DESC"]],
+  });
+};
+
+const updateUser = async (userId, updateData) => {
+  const user = await User.findByPk(userId);
+  if (user) {
+    return await user.update(updateData);
+  }
+  return null;
+};
+
+const deleteUser = async (userId) => {
+  const user = await User.findByPk(userId);
+  if (user) {
+    await user.destroy();
+    return true;
+  }
+  return false;
+};
+
+module.exports = {
+  createUser,
+  getUserById,
+  getAllUsers,
+  getRecentUsers,
+  updateUser,
+  deleteUser,
+};
