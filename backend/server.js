3448bc0836552ee1db0fc6243c0ee141b91c98d1
bbwark <devivobruno01@gmail.com>
2024-07-11 20:11:18 +0200

    api endpoints open in server

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use("/static", express.static(path.resolve(__dirname, "..", "frontend", "static")));
 
+// API endpoints
+app.use('/api/users', userController);
+app.use('/api/articles', articleController);
+
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "..", "frontend", "index.html"));
 });
