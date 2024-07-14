56adcc31e40be5ebe8be6f3cac5505ae4c950891
bbwark <devivobruno01@gmail.com>
2024-07-14 17:52:24 +0200

    ArticleShowcase and Article component fixed
    ArticleShowcase also checks the state of the user to modify the variables that build the article component, the article itself makes a REST call to retrieve the author name for the article since the author can change his name throughout the life of the application; another solution could have been modify the database SQL with some trigger functions to store always the name of the author alongside his id into the article table, but would have been over the purpose of the project

--- frontend/static/js/views/ArticleShowcase.js
+++ frontend/static/js/views/ArticleShowcase.js
@@ -1,22 +1,32 @@
 import AbstractView from "./AbstractView.js";
 import Article from "./components/Article.js";
 import HeaderShowcase from "./components/HeaderShowcase.js";
+import { state } from "../config.js";
 
 export default class extends AbstractView {
   constructor(params) {
     super(params);
     this.setTitle("Article Showcase");
-    this.articles = params.articles;
   }
 
   async getHtml() {
     const articleHtml = [];
-    for (const article in this.articles) {
+    let firstArticle = true;
+    for (const article of this.params) {
+      article.preview = firstArticle;
+      firstArticle = false;
+      if (article.authorId === state.userId || state.isAdmin) {
+        article.showEditDeleteButtons = true;
+      }
+
       const articleView = new Article(article);
       articleHtml.push(await articleView.getHtml());
     }
 
-    const headerView = new HeaderShowcase({ isLogged: true, isAdmin: true });
+    const headerView = new HeaderShowcase({
+      isLogged: state.isLogged,
+      isAdmin: state.isAdmin,
+    });
     const headerHtml = await headerView.getHtml();
 
     return `
--- frontend/static/js/views/components/Article.js
+++ frontend/static/js/views/components/Article.js
@@ -1,18 +1,21 @@
 import AbstractView from "../AbstractView.js";
+import rest from "../../rest.js";
 
 export default class extends AbstractView {
   constructor(params) {
     super(params);
-    this.article = params.article;
   }
 
   async getHtml() {
+    const data = await rest.getUserById(this.params.authorId);
+    const authorName = data.name;
+
     return `
             <div class="article">
                 <div class="article-header">
-                    <h3>${this.article.title}</h3>
+                    <h3>${this.params.title}</h3>
                     ${
-                      this.article.showEditDeleteButtons
+                      this.params.showEditDeleteButtons
                         ? `
                     <button class="edit-button">✏️</button>
                     <button class="delete-button">🗑️</button>`
@@ -20,13 +23,16 @@ export default class extends AbstractView {
                     }
                 </div>
                 <div class="article-details">
-                  <p>Author: ${this.article.author}</p>
-                  <p>Published on: ${this.article.publishedDate}</p>
-                  <p>Last modified: ${this.article.modifiedDate}</p>
-                  <p>Tags: ${this.article.tags.join(", ")}</p>
+                  <p>Author: ${!authorName ? "Account Deleted" : authorName}</p>
+                  <p>Published on: ${this.params.publishedDate}</p>
+                  <p>Last modified: ${this.params.modifiedDate}</p>
+                  <p>Tags: ${this.params.tags}</p>
                   ${
-                    this.article.preview
-                      ? `<p class="preview">${this.article.content.substring(0,50)}...</p>`
+                    this.params.preview
+                      ? `<p class="preview">${this.params.content.substring(
+                          0,
+                          50
+                        )}...</p>`
                       : ""
                   }
                 </div>
