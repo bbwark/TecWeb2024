e535f04765bee0db54ace4043173d891be14f6a0
bbwark <devivobruno01@gmail.com>
2024-07-14 21:16:02 +0200

    Persistence State handling

--- frontend/static/js/config.js
+++ frontend/static/js/config.js
@@ -7,20 +7,47 @@ const state = {
   isLogged: false,
   isAdmin: false,
   userId: null,
+  openedPage: 1,
+  articleModifying: 0,
   setLoggedInStatus(status) {
     this.isLogged = status;
+    this.saveState();
   },
   setAdminStatus(status) {
     this.isAdmin = status;
     if (status) this.isLogged = true;
+    this.saveState();
   },
   setUserId(userId) {
     this.userId = userId;
+    this.saveState();
   },
-  clearUser() {
+  clearState() {
     this.userId = null;
     this.isLogged = false;
     this.isAdmin = false;
+    this.openedPage = 1;
+    this.articleModifying = 0;
+    this.saveState();
+  },
+  setOpenedPage(page) {
+    this.openedPage = page;
+    this.saveState();
+  },
+  setArticleModifying(articleId) {
+    this.articleModifying = articleId;
+    this.saveState();
+  },
+
+  loadState() {
+    const savedState = localStorage.getItem("appState");
+    if (savedState) {
+      Object.assign(this, JSON.parse(savedState));
+    }
+  },
+
+  saveState() {
+    localStorage.setItem("appState", JSON.stringify(this));
   },
 };
 
