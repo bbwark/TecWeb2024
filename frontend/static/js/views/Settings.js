c206a3cef46b956088beb51ad4d75f8ec7b7e830
bbwark <devivobruno01@gmail.com>
2024-08-05 19:26:39 +0200

    Settings fixed

--- frontend/static/js/views/Settings.js
+++ frontend/static/js/views/Settings.js
@@ -1,64 +1,64 @@
 import AbstractView from "./AbstractView.js";
-import { state } from "../config.js";
+import { articleShowCaseState, state } from "../config.js";
 import ChangePassword from "./components/ChangePassword.js";
 import ChangeName from "./components/ChangeName.js";
 import CreateUser from "./components/CreateUser.js";
 import DeleteAccount from "./components/DeleteAccount.js";
 import UserList from "./components/UserList.js";
+import ArticleShowcase from "./ArticleShowcase.js";
+import rest from "../rest.js";
+import Login from "./Login.js";
 
 export default class extends AbstractView {
     constructor(params) {
         super(params);
         this.setTitle("Settings");
-        this.components = {
+        this.components = ["changePassword", "changeName", "createUser", "deleteAccount", "userList"];
+        const app = document.querySelector("#app");
+        if(!app.setSettingsContent) app.setSettingsContent = this.setSettingsContent;
+        if(!app.logout) app.logout = this.logout;
+    }
+
+    async getHtml() {
+        if(!state.isLogged) {
+            state.clearState();
+            history.pushState(null, null, "/login");
+            document.querySelector("#app").innerHTML = await new Login().getHtml();
+        } else {
+            return `
+                <div class="settings-container">
+                    <nav class="settings-nav">
+                        <ul>
+                            <li id="nav-change-password" onclick="app.setSettingsContent('${this.components[0]}')">Cambia Password</li>
+                            <li id="nav-change-name" onclick="app.setSettingsContent('${this.components[1]}')">Cambia Nome</li>
+                            ${state.isAdmin ? `<li id="nav-create-user" onclick="app.setSettingsContent('${this.components[2]}')">Crea Utente</li>` : ''}
+                            <li id="nav-delete-account" onclick="app.setSettingsContent('${this.components[3]}')">Elimina Account</li>
+                            ${state.isAdmin ? `<li id="nav-user-list" onclick="app.setSettingsContent('${this.components[4]}')">Visualizza Utenti</li>` : ''}
+                            <li id="nav-logout" onclick="app.logout()">Logout</li>
+                        </ul>
+                    </nav>
+                    <div id="settings-content"></div>
+                </div>
+            `;
+        }
+    }
+
+    async setSettingsContent(componentName) {
+        let components = {
             changePassword: new ChangePassword(),
             changeName: new ChangeName(),
             createUser: new CreateUser(),
             deleteAccount: new DeleteAccount(),
             userList: new UserList()
         };
+        document.querySelector("#settings-content").innerHTML = await components[componentName].getHtml();
     }
 
-    async getHtml() {
-        return `
-            <div class="settings-container">
-                <nav class="settings-nav">
-                    <ul>
-                        <li id="nav-change-password">Cambia Password</li>
-                        <li id="nav-change-name">Cambia Nome</li>
-                        ${state.isAdmin ? '<li id="nav-create-user">Crea Utente</li>' : ''}
-                        <li id="nav-delete-account">Elimina Account</li>
-                        ${state.isAdmin ? '<li id="nav-user-list">Visualizza Utenti</li>' : ''}
-                        <li id="nav-logout">Logout</li>
-                    </ul>
-                </nav>
-                <div class="settings-content" id="settings-content">
-                    <h1>Benvenuto nelle impostazioni</h1>
-                </div>
-            </div>
-        `;
-    }
-
-    async afterRender() {
-        document.getElementById('nav-change-password').addEventListener('click', () => this.loadComponent('changePassword'));
-        document.getElementById('nav-change-name').addEventListener('click', () => this.loadComponent('changeName'));
-        if (state.isAdmin) {
-            document.getElementById('nav-create-user').addEventListener('click', () => this.loadComponent('createUser'));
-            document.getElementById('nav-user-list').addEventListener('click', () => this.loadComponent('userList'));
-        }
-        document.getElementById('nav-delete-account').addEventListener('click', () => this.loadComponent('deleteAccount'));
-        document.getElementById('nav-logout').addEventListener('click', () => this.logout());
-    }
-
-    async loadComponent(componentName) {
-        const component = this.components[componentName];
-        document.getElementById('settings-content').innerHTML = await component.getHtml();
-        await component.afterRender();
-    }
-
-    logout() {
+    async logout() {
         state.clearState();
-        alert("Logged out successfully");
-        window.location.href = "/login";
+        history.pushState(null, null, "/");
+        state.setArticleShowcaseState(articleShowCaseState.ALL_ARTICLES);
+        state.setArticlesToShow(await rest.getRecentArticles(state.openedPage));
+        document.querySelector("#app").innerHTML = await new ArticleShowcase().getHtml();
     }
 }
