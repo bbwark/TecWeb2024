4cd0dfbe7d803944a181cd5668c71dd773400b23
bbwark <devivobruno01@gmail.com>
2024-08-24 22:59:18 +0200

    Token payload lightened

--- backend/controllers/authenticationController.js
+++ backend/controllers/authenticationController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const userService = require("../services/userService");
-const e = require("express");
+const { tokenPrivateKey } = require("../config");
 
 const authenticationController = express.Router();
 
@@ -12,12 +12,17 @@ authenticationController.post("/login", async (req, res) => {
   }
   try {
     if (await bcrypt.compare(req.body.password, user.password)) {
-      return res.status(200).send("Login successful");
+      const tokenPayload = {
+        id: user.id,
+        isAdmin: user.isAdmin,
+      };
+      const accessToken = jwt.sign(tokenPayload, tokenPrivateKey);
+      res.json({ accessToken });
     } else {
       return res.status(401).send("Invalid password");
     }
   } catch (error) {
-    return res.status(500).json({error: error.message});
+    return res.status(500).json({ error: error.message });
   }
 });
 
