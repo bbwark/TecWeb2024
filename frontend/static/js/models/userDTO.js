6dbd037c457c5c83787804f840d9fc8ceac97aa4
bbwark <devivobruno01@gmail.com>
2024-08-24 19:52:55 +0200

    Frontend model modified to hold password

--- frontend/static/js/models/userDTO.js
+++ frontend/static/js/models/userDTO.js
@@ -1,7 +1,8 @@
 export default class UserDTO {
-    constructor(id, username, name, isAdmin) {
+    constructor(id, username, password, name, isAdmin) {
         this.id = id;
         this.username = username;
+        this.password = password;
         this.name = name;
         this.isAdmin = isAdmin;
     }
