7174135ffac80b292aad83cf9e9a3d81975c6ca9
bbwark <devivobruno01@gmail.com>
2024-08-24 19:52:43 +0200

    hashing password on create and update user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const userService = require("../services/userService");
 
 const userController = express.Router();
 
 userController.post("/", async (req, res) => {
   try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashedPassword;
     const user = await userService.createUser(req.body);
     res.status(201).json(user);
   } catch (error) {
@@ -46,12 +49,18 @@ userController.get("/list/:page", async (req, res) => {
 
 userController.put("/:id", async (req, res) => {
   try {
-    const user = await userService.updateUser(req.params.id, req.body);
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res.status(404).json({ error: "User not found" });
+    const user = await userService.getUserById(req.params.id);
+    if (user === null) {
+      return res.status(404).json({ error: "User not found" });
     }
+
+    if (!(await bcrypt.compare(req.body.password, user.password))) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      req.body.password = hashedPassword;
+    }
+
+    const userUpdated = await userService.updateUser(req.params.id, req.body);
+    res.status(200).json(userUpdated);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
