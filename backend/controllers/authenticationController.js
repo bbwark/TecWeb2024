2f45201e7b9d54ec69cb9c866619d9615c26cebf
bbwark <devivobruno01@gmail.com>
2024-08-25 16:09:25 +0200

    Password Check method added

--- backend/controllers/authenticationController.js
+++ backend/controllers/authenticationController.js
@@ -27,4 +27,20 @@ authenticationController.post("/login", async (req, res) => {
   }
 });
 
+authenticationController.post("/passwordcheck", async (req, res) => {
+  const user = await userService.getUserById(req.body.id);
+  if (user === null) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.json(true);
+    } else {
+      res.json(false);
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 module.exports = authenticationController;
