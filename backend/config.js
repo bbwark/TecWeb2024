97f943744ccb09ae31411f10dc89e41a81db3808
bbwark <devivobruno01@gmail.com>
2024-08-25 16:12:08 +0200

    Hardcoded token removed

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,10 @@
-crypto = require('crypto');
+const { randomBytes } = require('crypto');
 
 const config = {
     pagingNumber: 10
 }
 
-// This is the private key used to sign the JWTs, it should be changed for deployment
-const tokenPrivateKey = crypto.randomBytes(64).toString('hex');
+const tokenPrivateKey = randomBytes(64).toString('hex');
 
 module.exports = {
     config,
