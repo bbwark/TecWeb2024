11564cd284530cba44732876c1e422b19c4d1bff
bbwark <devivobruno01@gmail.com>
2024-08-28 18:21:02 +0200

    Pagination component added to articleshowcase

--- frontend/static/js/views/ArticleShowcase.js
+++ frontend/static/js/views/ArticleShowcase.js
@@ -1,7 +1,9 @@
 import AbstractView from "./AbstractView.js";
 import Article from "./components/Article.js";
 import HeaderShowcase from "./components/HeaderShowcase.js";
-import { state } from "../config.js";
+import Pagination from "./components/Pagination.js";
+import { articleShowCaseState, config, state } from "../config.js";
+import rest from "../rest.js";
 
 export default class extends AbstractView {
   constructor(params) {
@@ -23,6 +25,10 @@ export default class extends AbstractView {
       articleHtml.push(await articleView.getHtml());
     }
 
+    const numberOfPages = await this.numberOfPages();
+    const paginationView = new Pagination({ currentPage: state.openedPage, totalPages: numberOfPages });
+    const paginationHtml = await paginationView.getHtml();
+
     const headerView = new HeaderShowcase({ isLogged: state.isLogged });
     const headerHtml = await headerView.getHtml();
 
@@ -31,6 +37,26 @@ export default class extends AbstractView {
             <div id="article-list">
                 ${articleHtml.join("")}
             </div>
+            ${paginationHtml}
         `;
+  }
+
+  async numberOfPages() {
+    let totalArticles = 0;
+    switch (state.articleShowCaseState) {
+      case articleShowCaseState.USER_ARTICLES:
+        totalArticles = await rest.getNumberOfArticlesByUserId(
+          state.userIdArticlesToShow
+        );
+        break;
+      case articleShowCaseState.TAG_ARTICLES:
+        totalArticles = await rest.getNumberOfArticlesByTag(state.openedTag);
+        break;
+      case articleShowCaseState.ALL_ARTICLES:
+      default:
+        totalArticles = await rest.getNumberOfArticles();
+        break;
+    }
+    return Math.ceil(totalArticles / config.numberOfArticlesPerPage);
   }
 }
