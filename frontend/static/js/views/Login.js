ba9f80ca58ebdb5b08d0b7b56c1c3533817cde65
bbwark <devivobruno01@gmail.com>
2024-08-31 19:43:32 +0200

    Login style improved

--- frontend/static/js/views/Login.js
+++ frontend/static/js/views/Login.js
@@ -18,19 +18,29 @@ export default class extends AbstractView {
       this.afterRender();
     }, 0);
     return `
-            <h1 class="text-2xl font-bold mb-4">Login</h1>
-            <div id="login-form" class="p-4 bg-white rounded shadow">
-              <div class="mb-4">
-                  <label for="username" class="block text-sm font-medium text-gray-700">Username:</label>
-                  <input type="text" id="username" name="username" class="mt-1 p-2 block w-full border border-gray-300 rounded" required>
+            <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+              <div class="max-w-md w-full space-y-8">
+                <div>
+                  <h1 class="text-3xl font-extrabold text-center text-gray-900">Login</h1>
+                </div>
+                <div id="login-form" class="mt-8 space-y-6 bg-white p-8 rounded-lg shadow-md">
+                  <div>
+                    <label for="username" class="block text-sm font-medium text-gray-700">Username:</label>
+                    <input type="text" id="username" name="username" class="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" required>
+                  </div>
+                  <div>
+                    <label for="password" class="block text-sm font-medium text-gray-700">Password:</label>
+                    <input type="password" id="password" name="password" class="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500" required>
+                  </div>
+                  <div>
+                    <button onclick="app.submitLogin()" id="login-button" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                      Login
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div class="mb-4">
-                  <label for="password" class="block text-sm font-medium text-gray-700">Password:</label>
-                  <input type="password" id="password" name="password" class="mt-1 p-2 block w-full border border-gray-300 rounded" required>
-              </div>
-              <button onclick="app.submitLogin()" id="login-button" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Login</button>
             </div>
-        `;
+            `;
   }
 
   async submitLogin() {
