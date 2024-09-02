c2401d4198900d538b66f6918602ebce4c6d7c90
bbwark <devivobruno01@gmail.com>
2024-09-02 17:31:12 +0200

    Number of users per page reduced

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ const { randomBytes } = require('crypto');
 
 const config = {
     numberOfArticlesPerPage: 10,
-    numberOfUsersPerPage: 10,
+    numberOfUsersPerPage: 8,
 }
 
 const tokenPrivateKey = randomBytes(64).toString('hex');
