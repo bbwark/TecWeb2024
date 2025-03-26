a150daa1a96b80472536a83da81ee06c99b8e4a9
Bruno De Vivo <devivobruno01@gmail.com>
2025-03-26 11:46:20 +0100

    Emptying login fields when submitting

--- frontend/static/js/views/Login.js
+++ frontend/static/js/views/Login.js
@@ -52,6 +52,9 @@ export default class extends AbstractView {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+    
     escapeHtml(username);
     escapeHtml(password);
 
