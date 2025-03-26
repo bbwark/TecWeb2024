42907b28c39a3e23f0ef39f064df7e662ee25b38
Bruno De Vivo <devivobruno01@gmail.com>
2025-03-26 17:15:15 +0100

    Mounting and Unmounting views added to router

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -27,6 +27,8 @@ const navigateTo = async (url) => {
   await router();
 };
 
+let currentView = null;
+
 const router = async () => {
   state.loadState();
 
@@ -43,7 +45,6 @@ const router = async () => {
     { path: "/settings", view: Settings },
   ];
 
-  // Test each route for potential match
   const potentialMatches = routes.map((route) => {
     return {
       route: route,
@@ -65,9 +66,20 @@ const router = async () => {
   if (match.route.path === "/") {
     await setArticlesToShowBasedOnState();
   }
-  let view = new match.route.view();
+  
+  if (currentView) {
+    currentView.onUnmount();
+  }
+  
+  const view = new match.route.view(getParams(match));
 
   document.querySelector("#app").innerHTML = await view.getHtml();
+  
+  view.onMount();
+  
+  currentView = view;
+  
+  window._appCurrentView = view;
 };
 
 window.addEventListener("popstate", router);
