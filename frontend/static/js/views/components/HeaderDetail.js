0b2db5d50b9720e16c4be6a3c953e042604580cd
Bruno De Vivo <devivobruno01@gmail.com>
2025-03-26 17:13:29 +0100

    Event Delegation added to HeaderDetail

--- frontend/static/js/views/components/HeaderDetail.js
+++ frontend/static/js/views/components/HeaderDetail.js
@@ -1,46 +1,116 @@
 import { state } from "../../config.js";
+import { navigateTo } from "../../index.js";
+import rest from "../../rest.js";
 import AbstractView from "../AbstractView.js";
-import Article from "./Article.js";
-import HeaderShowcase from "./HeaderShowcase.js";
 
 export default class extends AbstractView {
   constructor(params) {
     super(params);
+    
+    this.boundHandlers = {
+      click: this.handleClick.bind(this)
+    };
+  }
+  
+  onMount() {
     const app = document.querySelector("#app");
-    if (!app.goToSettings || !app.goToLogin) {
-      const instance = new HeaderShowcase();
-      app.goToSettings = instance.goToSettings;
-      app.goToLogin = instance.goToLogin;
-    }
-    if (!app.articleEditButton || !app.articleDeleteButton) {
-      const instance = new Article();
-      app.articleEditButton = instance.articleEditButton;
-      app.articleDeleteButton = instance.articleDeleteButton;
+    app.addEventListener('click', this.boundHandlers.click);
+    console.log("HeaderDetail mounted: event listeners added");
+  }
+  
+  onUnmount() {
+    const app = document.querySelector("#app");
+    app.removeEventListener('click', this.boundHandlers.click);
+    console.log("HeaderDetail unmounted: event listeners removed");
+  }
+  
+  handleClick(e) {
+    if (!document.getElementById('header-detail')) return;
+    
+    const target = e.target.closest('[data-action]');
+    if (!target) return;
+    
+    const action = target.dataset.action;
+    const articleId = parseInt(target.dataset.articleId);
+    
+    switch(action) {
+      case 'delete-article':
+        this.articleDeleteButton(articleId);
+        break;
+      case 'edit-article':
+        this.articleEditButton(articleId);
+        break;
+      case 'go-to-settings':
+        this.goToSettings();
+        break;
+      case 'go-to-login':
+        this.goToLogin();
+        break;
     }
   }
 
   async getHtml() {
     const isOwner = this.params.isOwner;
     return `
-        <div id="header" class="p-4 bg-gray-100 flex justify-end space-x-2">
+        <div id="header-detail" class="p-4 bg-gray-100 flex justify-end space-x-2">
             ${
               state.isLogged
                 ? `
                 ${
                   isOwner || state.isAdmin
                     ? `
-                    <button onclick="app.articleDeleteButton(${state.articleIdDetailOpened})" id="delete" class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">Delete</button>
-                    <button onclick="app.articleEditButton(${state.articleIdDetailOpened})" id="modify" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Modify</button>
+                    <button 
+                      data-action="delete-article" 
+                      data-article-id="${state.articleIdDetailOpened}" 
+                      class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">
+                      Delete
+                    </button>
+                    <button 
+                      data-action="edit-article" 
+                      data-article-id="${state.articleIdDetailOpened}" 
+                      class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                      Modify
+                    </button>
                 `
                     : ""
                 }
-                <button onclick="app.goToSettings()" id="settings" class="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400">Settings</button>
+                <button 
+                  data-action="go-to-settings" 
+                  class="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400">
+                  Settings
+                </button>
             `
                 : `
-                <button onclick="app.goToLogin()" id="login-button" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Login</button>
+                <button 
+                  data-action="go-to-login" 
+                  class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                  Login
+                </button>
             `
             }
         </div>
     `;
+  }
+
+  async articleDeleteButton(articleId) {
+    try {
+      await rest.deleteArticle(articleId);
+      await navigateTo("/");
+    } catch (error) {
+      console.error("Error deleting article:", error);
+    }
+  }
+
+  async articleEditButton(articleId) {
+    state.setArticleModifying(articleId);
+    await navigateTo("/modify-article");
+  }
+
+  async goToSettings() {
+    await navigateTo("/settings");
+  }
+
+  async goToLogin() {
+    await navigateTo("/login");
   }
 }
